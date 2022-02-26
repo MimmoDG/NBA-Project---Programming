1c6c667cb77e46f35ec86f90fc4b095f9af21c0e
MimmoDG <dome.alessandro@gmail.com>
2022-02-26 17:39:30 +0100

    Import some libraries

--- streamlit_NBA.py
+++ streamlit_NBA.py
@@ -1,4 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+import sklearn.metrics as skm
+import time
+
+
+
 st.header('NBA Project: Streamlit page for the Programming Project')
 st.write('Siamo molto contenti')
 #pd.read_csv('Nome dataset già pulito in csv')
