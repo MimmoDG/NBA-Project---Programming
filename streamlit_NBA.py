bc59f3ea6b028781b144a6b2ec456533bb81e6cf
MimmoDG <dome.alessandro@gmail.com>
2022-02-26 17:03:31 +0100

    Update in the streamlit

--- streamlit_NBA.py
+++ streamlit_NBA.py
@@ -1,4 +1,4 @@
 import streamlit as st
-st.header('Ciccio')
+st.header('NBA Project: Streamlit page for the Programming Project')
 st.write('Siamo molto contenti')
 #pd.read_csv('Nome dataset già pulito in csv')
