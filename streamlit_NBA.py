4cad58860dad2b8c81414e637091c54f2144921e
MimmoDG <dome.alessandro@gmail.com>
2022-03-01 16:16:25 +0100

    Update in the streamlit

--- streamlit_NBA.py
+++ streamlit_NBA.py
@@ -1,16 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
+import sklearn as sk
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 import sklearn.metrics as skm
 import time
+import streamlit.components.v1 as components
 
 #import the datasets
 
 Adv_Stats_1_df = pd.read_csv('Adv_Stats_1.csv')
 xG_Stats_1_df = pd.read_csv('xG_Stats_1.csv')
 LeBron_Injuries_df = pd.read_csv('LeBron_Injuries.csv')
+
+HtmlFile = open("https://www.basketball-reference.com/players/j/jamesle01.html", 'r', encoding='utf-8')
+LBJ_xG_Stats = HtmlFile.read() 
+print(LBJ_xG_Stats)
+components.html(LBJ_xG_Stats)
 
 st.title('NBA Project: Streamlit page for the Programming Project')
 
